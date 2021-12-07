58d56d911c68b9fc31c887f3e7f6705469bf3ae0
Marcelo-Molinas <federico.marcelo.molinas@gmail.com>
2021-12-07 08:42:04 -0300

    mongoose schemas added

--- server.js
+++ server.js
@@ -3,6 +3,28 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+})
+const {Schema} = mongoose
+
+const exercixeSchema = new Schema({
+  userID: {type: String, required:true},
+  description: String,
+  duration: Number,
+  date: Date,
+})
+
+const userSchema = new Schema({
+  userSchema: String,
+})
+
+const User =  mongoose.model('User', UserSchema)
+const Exercise = mongoose.model('Exercise', ExerciseSchema)
+
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
